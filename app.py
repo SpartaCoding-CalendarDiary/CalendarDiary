d62e5b86112fefec706a1ca0de761776ce2cf90c
yujinchoe <yujinchoe0214@gmail.com>
2021-10-19 23:56:01 +0900

    writeapi

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 from pymongo import MongoClient
-client = MongoClient('mongodb://test:test@디비서버주소', 27017)#서버주소는 git repository에는 올리지 않기
+client = MongoClient('mongodb://test:test@---------', 27017)#서버주소는 git repository에는 올리지 않기
 db = client.dbcalendardiary
 
 ## HTML을 주는 부분
@@ -10,27 +10,36 @@ db = client.dbcalendardiary
 def home():
    return render_template('calendar.html')
 
+@app.route('/write')
+def write():
+   return render_template('write.html')
+
 ## API역할을 하는 부분
 #유저가 작성한 글을 저장한다(front->server)
-# @app.route('/diaries', methods=['POST'])#은찬
-# def write_diary():
-#     글변수명 = request.form['글_give']
-#     사진변수명 = request.form['사진_give']
-#     날짜변수명 = request.form['날짜_give']
-#
-#     doc = {
-#         '글변수명':글_receive,
-#         '사진변수명':사진_receive,
-#         '날짜변수명':날짜_receive
-#     }
-#
-#     db.디비콜렉션명.insert_one(doc)
-#
-#     return jsonify({'msg': '저장 완료!'})
+@app.route('/diaries', methods=['POST'])  # 은찬
+
+
+def write_diary():
+    text_receive = request.form['text_give']
+    img_receive = request.form['img_give']
+    date_receive = request.form['date_give']
+    name_receive = request.form['name_give']
+
+    doc = {
+        'text': text_receive,
+        'img': img_receive,
+        'date': date_receive,
+        'name': name_receive
+    }
+
+    db.diaries.insert_one(doc)
+
+    return jsonify({'msg': '저장 완료!'})
+
 #
 #해당 날짜(key)의 글을 불러온다(server->front)
-#이 API는 글을 실제로 불러올 때 & 글을 저장하기 전 해당 날짜의 글 작성 여부를 확인할 때. 총 2번 쓰일 것 같음
-# @app.route('/diaries', methods=['GET'])#은찬
+#이 API는 글을 실제로 불러올 때
+# @app.route('/diaries', methods=['GET']) #은찬
 # def show_diary():
 #     변수명 = db.디비콜렉션명.find_one({'찾을key': 찾을value})
 #     return jsonify({'변수명s': 변수명})
