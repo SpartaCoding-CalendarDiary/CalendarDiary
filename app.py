207bd666c3ffb7995998a4a08d53092593e58b31
yujinchoe <yujinchoe0214@gmail.com>
2021-10-28 23:42:09 +0900

    the result of update and del

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from pymongo import MongoClient #dumps는 pymongo에서 제공해주는 util
 from bson import json_util
 
 client = MongoClient(f'mongodb://{DATABASES.get("username")}:{DATABASES.get("password")}@{DATABASES.get("address")}', 27017)
-
 db = client.dbcalendardiary
 
 ## HTML을 주는 부분
@@ -32,6 +31,7 @@ def menubar():
 def aboutus():
    return render_template('aboutus.html')
 
+
 ## API역할을 하는 부분
 #유저가 작성한 글을 저장한다(front->server)
 @app.route('/diaries', methods=['POST'])  # 은찬
@@ -46,7 +46,7 @@ def write_diary():
         'date': date_receive,
         'name': name_receive
     }
-    print(doc)
+    #print(doc)
     # DB에 해당 날짜에 저장된 사진 있나 확인하기 위해 DB에서 date를 조건으로 돌아오는 객체가 있나 확인.
     result = db.diaries.find_one({'date': date_receive}, {'_id': False})
 
@@ -58,6 +58,7 @@ def write_diary():
     else:
         return jsonify({'msg': '저장실패'})#프론트-글쓰기페이지에서 저장실패라고 alert 띄워야.
 
+
 #해당 날짜(key)의 글을 불러온다(server->front)
 #이 API는 글을 실제로 불러올 때
 #test - http://localhost:5000/diaries?date_give=2021-10-01
@@ -68,6 +69,7 @@ def show_diary():
     diary = json.loads(json_util.dumps(diary))
     return ({'diary': diary})
 
+
 #해당 날짜의 사진을 불러온다(server->front)
 @app.route('/calendar', methods=['GET'])
 def read_calendar():
@@ -75,20 +77,35 @@ def read_calendar():
     #print(allPic)
     return jsonify({'allPics': allPic})
 
+
 #다이어리 삭제
-@app.route('/delete', methods=['DELETE'])
+@app.route('/delete', methods=['GET'])
 def delete_diary():
     date_receive = request.args.get('date_give')
-    db.diaries.delete_one({'date': date_receive})
-    return jsonify({'msg': "삭제완료"})
+    #print(date_receive)
+    result = db.diaries.delete_one({'date': date_receive})
+    print(result)
+    if result is not None:
+        msg = "삭제완료"
+    else:
+        msg = "삭제실패"
+    return jsonify({'msg': msg})
+
 
 #다이어리 수정
-@app.route('/update', methods=['PUT'])
+@app.route('/update', methods=['GET'])
 def update_diary():
+    print(request)
+    print(request.args)
     date_receive = request.args.get('date_give')
     text_receive = request.args.get('text_give')
-    db.diaries.update_one({'date':date_receive},{'$set':{'text':text_receive}})
-    return jsonify({'msg': "수정완료"})
+    result = db.diaries.update_one({'date':date_receive},{'$set':{'text':text_receive}})
+    if result is not None:
+        msg = "수정완료"
+    else:
+        msg = "수정실패"
+    return jsonify({'msg': msg})
+
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
